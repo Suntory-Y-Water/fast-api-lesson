dae288781cc4059e9d27fd68468c97e0b70d8083
sui <1202ayasnppk@gmail.com>
2024-01-02 14:46:48 +0900

    fix: Chapter12

--- api/routers/task.py
+++ api/routers/task.py
@@ -1,13 +1,13 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
-from api.db import get_db
-import api.cruds.task as task_crud
+
 import api.schemas.task as task_schema
+import api.cruds.task as task_crud
+from api.db import get_db
 
 router = APIRouter()
 
 
-# 後々データベース接続用のtask_modelを作成するのでスキーマ定義用のtaskを区別する。
 @router.get("/tasks", response_model=list[task_schema.Task])
 async def list_tasks(db: AsyncSession = Depends(get_db)):
     return await task_crud.get_tasks_with_done(db)
@@ -19,10 +19,18 @@ async def create_task(task_body: task_schema.TaskCreate, db: AsyncSession = Depe
 
 
 @router.put("/tasks/{task_id}", response_model=task_schema.TaskCreateResponse)
-async def update_task(task_id: int, task_body: task_schema.TaskCreate):
-    return task_schema.TaskCreateResponse(id=task_id, **task_body.model_dump())
+async def update_task(task_id: int, task_body: task_schema.TaskCreate, db: AsyncSession = Depends(get_db)):
+    task = await task_crud.get_task(db, task_id=task_id)
+    if task is None:
+        raise HTTPException(status_code=404, detail="Task not found")
+
+    return await task_crud.update_task(db, task_body, original=task)
 
 
 @router.delete("/tasks/{task_id}", response_model=None)
-async def delete_task(task_id: int):
-    pass
+async def delete_task(task_id: int, db: AsyncSession = Depends(get_db)):
+    task = await task_crud.get_task(db, task_id=task_id)
+    if task is None:
+        raise HTTPException(status_code=404, detail="Task not found")
+
+    return await task_crud.delete_task(db, original=task)
